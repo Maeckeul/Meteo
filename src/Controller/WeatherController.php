b671dbca1e7ea3f5a8b81ba9c012b1d7c5bc2337
Maeckeul <cmaeckeul@gmail.com>
2020-08-17 16:48:38 +0200

    :construction: selectCity() created and partial _widget Twig

--- src/Controller/WeatherController.php
+++ src/Controller/WeatherController.php
@@ -2,10 +2,52 @@
 
 namespace App\Controller;
 
+use App\Model\WeatherModel;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WeatherController extends AbstractController {
+
+    private $session;
+    private $weatherModel;
+
+    public function __construct(SessionInterface $session, WeatherModel $weatherModel) {
+        $this->session = $session;
+        $this->weatherModel = $weatherModel;
+    }
+
+    /**
+     * Page d'acceuil
+     * 
+     * @Route("/", name="homepage")
+     */
+    public function index() {
+
+        $weatherData = $this->weatherModel->getWeatherData();
+
+        return $this->render("weather/homepage.html.twig", [
+            'weatherData' => $weatherData,
+        ]);
+    }
+
+    /**
+     * Selection de la ville
+     * 
+     * @Route("/city/{id}/select", name="city_select", requirements={"id" = "\d+"})
+     */
+    public function selectCity($id) {
+
+        $weatherData = $this->weatherModel->getWeatherByCityIndex($id);
+
+        if(!$weatherData) {
+            throw $this->createNotFoundException("Cette ville n'existe pas !");
+        }
+
+        $this->session->set('selected_city', $weatherData);
+
+        return $this->redirectToRoute('homepage');
+    }
 
     /**
      * Page Météo des Montagnes
@@ -14,6 +56,6 @@ class WeatherController extends AbstractController {
      */
     public function montagneWeather() {
 
-        return $this->render('paysages/montagne.html.twig');
+        return $this->render('weather/montagne.html.twig');
     }
 }
